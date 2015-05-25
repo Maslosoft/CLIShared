b8575e954d227d0fd2724a3280338cba7ac11890
Peter Maselkowski <pmaselkowski@gmail.com>
2015-05-25 18:38:47 +0200

    +~ Draft

---
+++ src/ConfigDetector.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Maslosoft\Cli\Shared;
+
+/**
+ * DRAFT
+ */
+class ConfigDetector
+{
+	public function getRootPath()
+	{
+	// Return current running application root dir. This must work on cli and http
+	}
+	
+	public function getVendorPath()
+	{
+	// Get vendor path, similarly as getRootPath
+	}
+}
---
+++ src/ConfigReader.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Maslosoft\Cli\Shared;
+
+/**
+ * DRAFT
+ * This should read config.yml on cli, and write it to `generated` folder as php file.
+ * From php this should only read php config. Should allow uniform property-like access
+ */
+class ConfigReader
+{
+	public function __construct($basename)
+	{
+	
+	}
+}
