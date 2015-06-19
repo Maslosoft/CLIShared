03c11cb0ad9d8b60b920b0f3716e18c4e306b180
Peter Maselkowski <pmaselkowski@gmail.com>
2015-06-19 15:11:28 +0200

    * other path accessors (somewhat stub)

--- src/Helpers/PhpExporter.php
+++ src/Helpers/PhpExporter.php
@@ -18,9 +18,9 @@ class PhpExporter
 	public static function export($data, $header = '')
 	{
 		$template = <<<TPL
-<?php
-	// %s
-	return %s;
+<?php // %s
+return %s;
+
 TPL;
 		return sprintf($template, $header, var_export($data, true));
 	}
