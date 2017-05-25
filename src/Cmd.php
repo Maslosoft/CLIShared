fd6f235688713054ffbe33821beada99eb0bcea5
Peter Maselkowski <pmaselkowski@gmail.com>
2017-05-25 12:34:23 +0200

    * Class headers

--- src/Cmd.php
+++ src/Cmd.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * This software package is licensed under `AGPL, Commercial` license[s].
+ *
+ * @package maslosoft/cli-shared
+ * @license AGPL, Commercial
+ *
+ * @copyright Copyright (c) Peter Maselkowski <pmaselkowski@gmail.com>
+ */
+
 namespace Maslosoft\Cli\Shared;
 
 /**
