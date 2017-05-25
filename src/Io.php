dccf9075de81b8ed3770626756d965adaac615c4
Peter Maselkowski <pmaselkowski@gmail.com>
2017-05-25 12:28:19 +0200

    + tempDir function

---
+++ src/Io.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Maslosoft\Cli\Shared;
+
+/**
+ * Io
+ *
+ * @author Piotr Maselkowski <pmaselkowski at gmail.com>
+ */
+class Io
+{
+
+	/**
+	 * Create temporary directory
+	 *
+	 * @param string $dir The directory where the temporary filename will be created.
+	 * @param string $prefix The prefix of the generated temporary filename. Windows uses only the first three characters of prefix.
+	 *
+	 * @return string The new temporary filename (with path), or FALSE on failure.
+	 */
+	public static function tempDir($dir, $prefix = '')
+	{
+		$filename = tempnam($dir, $prefix);
+		$dirname = $filename . 'dir';
+		// Silence out errors to get return value of mkdir
+		$level = error_reporting();
+		error_reporting(0);
+		if (!mkdir($dirname))
+		{
+			return false;
+		}
+		unlink($filename);
+		error_reporting($level);
+		return $dirname;
+	}
+
+}
