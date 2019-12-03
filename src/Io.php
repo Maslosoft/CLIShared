34c1ef64984495b892580604b9bd82f87408ece4
Peter Maselkowski <office@maslosoft.com>
2019-12-03 17:30:28 +0100

    + Added xcopy function

--- src/Io.php
+++ src/Io.php
@@ -73,5 +73,48 @@ class Io
 		error_reporting($level);
 		return $dirname;
 	}
+	/**
+	 * Copy a file, or recursively copy a folder and its contents
+	 * @author      Aidan Lister <aidan@php.net>
+	 * @version     1.0.1
+	 * @link        http://aidanlister.com/2004/04/recursively-copying-directories-in-php/
+	 * @link 		https://stackoverflow.com/a/12763962/5444623
+	 * @param       string   $source    Source path
+	 * @param       string   $dest      Destination path
+	 * @param       int      $permissions New folder creation permissions
+	 * @return      bool     Returns true on success, false on failure
+	 */
+	function xcopy($source, $dest, $permissions = 0755)
+	{
+		// Check for symlinks
+		if (is_link($source)) {
+			return symlink(readlink($source), $dest);
+		}
 
+		// Simple copy for a file
+		if (is_file($source)) {
+			return copy($source, $dest);
+		}
+
+		// Make destination directory
+		if (!is_dir($dest)) {
+			self::mkdir($dest, $permissions);
+		}
+
+		// Loop through the folder
+		$dir = dir($source);
+		while (false !== $entry = $dir->read()) {
+			// Skip pointers
+			if ($entry === '.' || $entry === '..') {
+				continue;
+			}
+
+			// Deep copy directories
+			self::xcopy("$source/$entry", "$dest/$entry", $permissions);
+		}
+
+		// Clean up
+		$dir->close();
+		return true;
+	}
 }
