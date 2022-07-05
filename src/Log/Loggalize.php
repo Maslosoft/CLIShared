34d0fa9b790f987fa0ec45418dd93736c2dc7e7d
Peter Maselkowski <office@maslosoft.com>
2022-07-05 20:32:34 +0200

    + Loggalize log helper

---
+++ src/Log/Loggalize.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Maslosoft\Cli\Shared\Log;
+
+use Maslosoft\Cli\Shared\Log\Logger;
+use Psr\Log\LoggerAwareInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use function assert;
+
+class Loggalize
+{
+	private static Logger $logger;
+	public static function with(object $object, OutputInterface $output): void
+	{
+		if($object instanceof LoggerAwareInterface)
+		{
+			self::$logger = new Logger($output);
+			$object->setLogger(self::$logger);
+		}
+	}
+
+	public static function get():Logger
+	{
+		assert(self::$logger instanceof Logger);
+		return self::$logger;
+	}
+}
