8efe95355b3ffe94ec166b02e9352bf59eb8f159
Peter Maselkowski <office@maslosoft.com>
2023-03-29 14:30:41 +0200

    - Removed dead code

--- src/Adapters/Internal/PhpRuntimeAdapter.php
+++ src/Adapters/Internal/PhpRuntimeAdapter.php
@@ -19,24 +19,15 @@ use Maslosoft\Cli\Shared\Helpers\PhpExporter;
 
 class PhpRuntimeAdapter extends PhpAdapter
 {
-
-	private $_reader = null;
-
-	public function __construct(ConfigReader $reader)
-	{
-		// $reade param is only to limit this class usage to internal usage by ConfigReader
-		$this->_reader = $reader;
-	}
-
 	public function read($basename)
 	{
 		return parent::read('runtime/' . $basename);
 	}
 
-	public function write($basename, $configuration)
+	public function write($basename, $configuration): false|int
 	{
 		$text = PhpExporter::export($configuration, 'Auto generated, any changes will be lost');
-		FileIO::write('runtime/' . $basename . '.php', $text);
+		return FileIO::write('runtime/' . $basename . '.php', $text);
 	}
 
 }
